91975aee6b21a610c144cfa0a975336af875dbc2
adevianin <remba93@gmail.com>
2015-04-25 23:20:58 +0300

    added sending results to exchange results after converting

--- src/AppBundle/Consumers/ConvertAudioConsumer.php
+++ src/AppBundle/Consumers/ConvertAudioConsumer.php
@@ -7,15 +7,30 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class ConvertAudioConsumer implements ConsumerInterface
 {
+    private $resultsProducer;
+
+    public function __construct($resultsProducer)
+    {
+        $this->resultsProducer = $resultsProducer;
+    }
+
     public function execute(AMQPMessage $msg)
     {
         $data = unserialize($msg->body);
         $tempFilePath = __DIR__.'/../../../temp/'.$data['fileName'];
+        $convertedFilePath = $tempFilePath.'.'.$data['format'];
 
         file_put_contents($tempFilePath, $data['fileContent']);
 
         exec(sprintf('ffmpeg -i %s %s.%s', $tempFilePath, $tempFilePath, $data['format']));
 
+        $this->resultsProducer->publish(serialize(array(
+            'fileContent' => file_get_contents($convertedFilePath),
+            'fileName' => $data['fileName'],
+            'format' => $data['format'],
+        )));
+
         unlink($tempFilePath);
+        unlink($convertedFilePath);
     }
 }
